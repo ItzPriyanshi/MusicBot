0771e161b3c0fed540fc6e716e3a4c5ae18345ce
Priyanshi Kaur <sasmitapattnayak104@gmail.com>
2025-05-13 09:12:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,39 +1,44 @@
 import re
+from os import getenv
+
+from dotenv import load_dotenv
 from pyrogram import filters
 
-API_ID = 23497256
-API_HASH = "9b55ac125b107ef2a544b520aeca0823"
-BOT_TOKEN = "7665256594:AAHqDwAKy0IzDTjA13-wgnNPv22CxtS8g9E"
-MONGO_DB_URI = "mongodb+srv://priyanshikaurji:priyanshikaurji@cluster0.kcbnt.mongodb.net/TeleBot?retryWrites=true&w=majority&appName=Cluster0"
-DURATION_LIMIT_MIN = 60
-LOGGER_ID = -1002474197899
-OWNER_ID = 6388959089
+load_dotenv()
 
-HEROKU_APP_NAME = None
-HEROKU_API_KEY = None
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+BOT_TOKEN = getenv("BOT_TOKEN")
+MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
+LOGGER_ID = int(getenv("LOGGER_ID", "-1002474197899"))
+OWNER_ID = int(getenv("OWNER_ID", "6388959089"))
 
-UPSTREAM_REPO = "https://github.com/AnonymousX1025/AnonXMusic"
-UPSTREAM_BRANCH = "master"
-GIT_TOKEN = None
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-SUPPORT_CHANNEL = "https://t.me/GlobalUpdateQueen"
-SUPPORT_CHAT = "https://t.me/GlobalChatGroupTG"
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/AnonymousX1025/AnonXMusic")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-AUTO_LEAVING_ASSISTANT = False
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/GlobalUpdateQueen")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/GlobalChatGroupTG")
 
-SPOTIFY_CLIENT_ID = "41dd52e608ee4c4ba8b196b943db9f73"
-SPOTIFY_CLIENT_SECRET = "5c7b438712b04d0a9fe2eaae6072fa16"
+AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False").lower() == "true"
 
-PLAYLIST_FETCH_LIMIT = 25
+SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
+SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
 
-TG_AUDIO_FILESIZE_LIMIT = 104857600
-TG_VIDEO_FILESIZE_LIMIT = 1073741824
+PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
 
-STRING1 = "1BVtsOKEBuyUaKHiB6H-KHtLwAwUKitAYWNE35KQzEedaoniWxYIXfH4uswuUb8lPxlAYxHSW7wkTts_LEJqdAOO_QYkAbSupL84zR8fKlr1Lj2PXtc4c0fJ9Zd3mE4YD9c0Bjx2bOe0tEoWvAS35B4_TW_JYA86-IzAe_1okrFiBxL5R9L_tAziI9X-IibNnMYDa2Y5-FNnV6WRKLtz2T40FZ7t6SO9y-bTlw7b23iPUTZjaLp99-nHKM7gZUkNtUMt0NMwYkCW4JtRBSRtnYCckLYAZscWuuxe7iGGFECdO0Q3RzWxadFXMpsY6mjIYgb6e0F_qn0ypaytPng5CjKaI-yiJbTE="
-STRING2 = None
-STRING3 = None
-STRING4 = None
-STRING5 = None
+TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
+TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
+
+STRING1 = getenv("STRING_SESSION1", None)
+STRING2 = getenv("STRING_SESSION2", None)
+STRING3 = getenv("STRING_SESSION3", None)
+STRING4 = getenv("STRING_SESSION4", None)
+STRING5 = getenv("STRING_SESSION5", None)
 
 BANNED_USERS = filters.user()
 adminlist = {}
@@ -42,8 +47,8 @@ votemode = {}
 autoclean = []
 confirmer = {}
 
-START_IMG_URL = "https://files.catbox.moe/2dve48"
-PING_IMG_URL = "https://files.catbox.moe/2dve48"
+START_IMG_URL = getenv("START_IMG_URL", "https://files.catbox.moe/2dve48")
+PING_IMG_URL = getenv("PING_IMG_URL", "https://files.catbox.moe/2dve48")
 PLAYLIST_IMG_URL = "https://files.catbox.moe/2dve48"
 STATS_IMG_URL = "https://files.catbox.moe/2dve48"
 TELEGRAM_AUDIO_URL = "https://files.catbox.moe/2dve48"
